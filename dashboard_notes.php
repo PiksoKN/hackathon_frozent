04d96c4808eaad14e442d199e1fa0786c9544c2b
Kamil Nowak <kamil.nowak.tbg@gmail.com>
2019-03-29 21:00:45 +0100

    Do notatek

--- dashboard_notes.php
+++ dashboard_notes.php
@@ -4,14 +4,42 @@
     <title>Index</title>
     <script src="script.js"></script>
     <link href="css/dashboard.css" rel="stylesheet" type="text/css">
+    <link href="css/notes.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="data\font-awesome\css\font-awesome.min.css">
 </head>
 
 <body>
+    <!-- Logo -->
     <div class="logo-wrap">
         <img src="data/logo-small.png" class="logo">
     </div>
-    <div class="header"></div>
-    <div class="leader"></div>
+    
+    <!-- Header -->
+    <div class="header">
+        <div class="header-search">
+            <i class='fa fa-search header-search-icon' aria-hidden='true'></i>
+            <input type="text" placeholder="Search..." class="header-search-input">
+        </div>
+        <div class="header-button-wrap">
+            <div class="header-button"><i class="fa fa-file-text-o" aria-hidden="true"></i></div>
+            <div class="header-button"><i class="fa fa-gavel" aria-hidden="true"></i></div>
+            <div class="header-button"><i class="fa fa-cog" aria-hidden="true"></i></div>
+        </div>
+    </div>
+    
+    <!-- Left Header -->
+    <div class="leader">
+        
+    </div>
+    
+    <!-- Content -->
+    <div class="content">
+
+    <div class="notes">
+      <h2 class="note">TEKST</h2>
+      <h5 class="note">23.08.19</h5>
+    </div>
+        
+    </div>
 </body>
 </html>
-
