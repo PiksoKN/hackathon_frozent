aa38b708ff6d184265a8a08aa6204247475b952f
Holo <holo@frozent.pl>
2019-03-29 20:29:17 +0100

    serek

--- dashboard.php
+++ dashboard.php
@@ -4,14 +4,33 @@
     <title>Index</title>
     <script src="script.js"></script>
     <link href="css/dashboard.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="data\font-awesome\css\font-awesome.min.css">
 </head>
 
 <body>
+    <!-- Logo -->
     <div class="logo-wrap">
         <img src="data/logo-small.png" class="logo">
     </div>
-    <div class="header"></div>
-    <div class="leader"></div>
+    
+    <!-- Header -->
+    <div class="header">
+        <div class="header-search">
+            <i class='fa fa-search' aria-hidden='true' class="header-search-icon"></i>
+            <input type="text" placeholder="Search..." class="header-search-input">
+        </div>
+        
+    </div>
+    
+    <!-- Left Header -->
+    <div class="leader">
+        
+    </div>
+    
+    <!-- Content -->
+    <div class="content">
+        
+    </div>
 </body>
 </html>
 
