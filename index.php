663e3b9bbb84c7d484b8e45aaa9dd0fd2053ac44
Holo <holo@frozent.pl>
2019-03-29 18:36:17 +0100

    testujemy dalej

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+    <script src="script.js"></script>
+    <link href="index.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="wrap">
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+</body>
+</html>
+
