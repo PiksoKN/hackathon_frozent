5519118b12e4fe7c55957d343310ffd115d57d90
Kubus <kartovel123321@gmail.com>
2019-03-30 02:26:41 +0100

    odnosniki

--- index.php
+++ index.php
@@ -20,17 +20,17 @@
         <div class="wrap">
 
             <!-- Box 1 -->
-            <div class="box">
+            <a href="dashboard.php"> <div class="box">
                 <div class="photo1"></div>
                 <div class="box-content1">
                     <div class="cover1"></div>
                     <div class="napisS1">Student / Uczeń</div>
                     <div class="napisD1">Konto dla osób uczących się</div> 
                 </div>
-            </div>
+            </div> </a>
 
             <!-- Box 2 -->
-            <div class="box">
+             <a href="dashboard.php"> <div class="box">
                 <div class="photo2"></div>
                 <div class="box-content2">
                     <div class="cover2"></div>
@@ -38,7 +38,7 @@
                     <div class="napisD2">Konto dla nauczycieli</div>
                 </div>
             </div>
-        </div>
+        </div> </a>
 
         <!-- Logout -->
         <div class="footer">
