5766cf4e8db6f64bbcb86c22a92c13a13e0316ec
Kamil Nowak <kamil.nowak.tbg@gmail.com>
2019-03-30 02:21:46 +0100

    poprawne tnote

--- notes.php
+++ notes.php
@@ -63,13 +63,13 @@
     <div class="leader">
         <div class="leader-button-wrap">
             <a href="dashboard.php">
-                <div class="leader-button leader-button-checked">
+                <div class="leader-button">
                     <div class="leader-label"><div class="leader-label-arrleft"></div><div class="leader-label-arrright"></div>DASHBOARD</div>
                     <i class="fa fa-user-o" aria-hidden="true"></i>
                 </div>
             </a>
             <a href="notes.php">
-                <div class="leader-button">
+                <div class="leader-button leader-button-checked">
                     <div class="leader-label"><div class="leader-label-arrleft"></div><div class="leader-label-arrright"></div>NOTES</div>
                     <i class="fa fa-files-o" aria-hidden="true"></i>
                 </div>
