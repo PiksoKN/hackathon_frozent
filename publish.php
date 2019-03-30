d2855c5503a2608651d1819dc737cc720a65be0e
Holo <holo@frozent.pl>
2019-03-30 06:05:02 +0100

    readowanie

--- publish.php
+++ publish.php
@@ -1,9 +1,15 @@
 <?php
+// Check if file exists
 $i = 0;
 while(file_exists("notes/" . $_POST["title"] . $i . ".txt")){
     $i += 1;
 }
+
+// Open file and write whole text
 $file = fopen("notes/" . $_POST["title"] . $i . ".txt", "w");
 fwrite($file, $_POST["noteText"]);
 fclose($file);
+
+// Redirect to all notes
+echo "<script type='text/javascript'>window.location = 'http://localhost/notes.php';</script>";
 ?>
---
+++ readnote.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+    <script src="dashboard.js"></script>
+    <link href="css/createnote.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="data\font-awesome\css\font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+
+<?php
+    $opened = file_get_contents("notes/" . $_GET["id"] . ".txt");
+?>
+
+<body>
+    <!-- Logo -->
+    <div class="logo-wrap">
+        <img src="data/logo-small.png" class="logo">
+    </div>
+    
+    <!-- Header -->
+    <div class="header">
+        <div class="header-search">
+            <div class="header-caption">Create New Note</div>
+            <div class="header-splitter"></div>
+            <i class='fa fa-search header-search-icon' aria-hidden='true'></i>
+            <input type="text" placeholder="Search..." class="header-search-input">
+        </div>
+        <div class="header-button-wrap">
+            <div class="header-button"><i class="fa fa-files-o" aria-hidden="true"></i></div>
+            <div class="header-splitter"></div>
+            <div class="header-button-av"><i class="fa fa-cog" aria-hidden="true"></i></div>
+            <a href="logout.php"><div class="header-button-av"><i class="fa fa-sign-out" aria-hidden="true"></i></div></a>
+            <div class="header-avatar" style="background-image: url(http://res.frozent.pl/sdadsd.jpg)"></div>
+        </div>
+    </div>
+    
+    <!-- Left Header -->
+    <div class="leader">
+        <div class="leader-button-wrap">
+            <a href="dashboard.php">
+                <div class="leader-button">
+                    <div class="leader-label"><div class="leader-label-arrleft"></div><div class="leader-label-arrright"></div>DASHBOARD</div>
+                    <i class="fa fa-user-o" aria-hidden="true"></i>
+                </div>
+            </a>
+            <a href="notes.php">
+                <div class="leader-button">
+                    <div class="leader-label"><div class="leader-label-arrleft"></div><div class="leader-label-arrright"></div>NOTES</div>
+                    <i class="fa fa-files-o" aria-hidden="true"></i>
+                </div>
+            </a>
+            <a href="createnote.php">
+                <div class="leader-button leader-button-checked">
+                    <div class="leader-label"><div class="leader-label-arrleft"></div><div class="leader-label-arrright"></div>CREATE NEW NOTE</div>
+                    <i class="fa fa-plus-square-o" aria-hidden="true"></i>
+                </div>
+            </a>
+        </div>
+    </div>
+    
+    <!-- Content -->
+    <div class="content">
+        <div class="content-writepanel"><?php echo $opened; ?></div>
+    </div>
+</body>
+</html>
+
